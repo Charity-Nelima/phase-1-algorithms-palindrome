5e8f7ba20fee2732b24a84288c53f946a7562b9d
Charity Nelima <wa.nelimacharity@gmail.com>
2022-09-14 16:42:05 +0300

    challenge completed

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const arrOfCharacters = word.split('')
+  const arrLength = arrOfCharacters.length
+  const numbOfCharactersToMatch = Math.floor(arrLength/2)
+  //check to see if first character equals last character
+  //check to see if second character equals second to last character
+  //continue until there are no more characters
+  for (let i=0; i < numbOfCharactersToMatch; i++) {
+    if (arrOfCharacters[i] === arrOfCharacters[arrLength - 1 - i]) {
+      //console.log('letters match')
+    }
+    //if any of the above is false, return false
+    else {
+      return false
+    }
+  }
+  //if everything is true, return true
+  return true
 }
 
-/* 
+isPalindrome('treehouse')
+
+
+
+/*
+ 
   Add your pseudocode here
 */
 
